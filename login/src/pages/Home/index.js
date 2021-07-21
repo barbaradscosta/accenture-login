f0ad5f0520d619f4b574861744d8f39cad895f14
Barbara Costa <barbaradscosta14@gmail.com>
2021-07-21 18:03:05 -0300

    first commit

---
+++ login/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '../../components/Container';
+
+// import { Container } from './styles';
+
+function Home() {
+  return (
+    <Container
+      title="Home"
+      size="lg"
+    >
+
+    </Container>
+  );
+}
+
+export default Home;
